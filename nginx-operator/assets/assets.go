42a464333809f1e50d59950e53612b3115c84e0e
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-01-03 20:09:48 -0300

    generate manifests

---
+++ nginx-operator/assets/assets.go
@@ -0,0 +1,45 @@
+package assets
+
+import (
+	"embed"
+
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+)
+
+var (
+	//go:embed manifests/*
+	manifests embed.FS
+
+	appsScheme = runtime.NewScheme()
+	appsCodecs = serializer.NewCodecFactory(appsScheme)
+)
+
+func init() {
+	if err := appsv1.AddToScheme(appsScheme); err != nil {
+		panic(err)
+	}
+}
+
+func GetDpeploymentFromFile(name string) *appsv1.Deployment {
+	deploymentBytes, err := manifests.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+
+	deploymentObject, err := runtime.Decode(
+		appsCodecs.UniversalDecoder(appsv1.SchemeGroupVersion),
+		deploymentBytes,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	deployment, ok := deploymentObject.(*appsv1.Deployment)
+	if !ok {
+		panic("unreachable: unable to convert value to *appsv1.Deployment")
+	}
+
+	return deployment
+}
